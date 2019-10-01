e76854dc435e6bc3a01380a302c5075aa7d4b5a2
jay <jay@contensive.com>
2019-10-01 08:58:36 -0400

    previous changes not committed - mostly folderr renaming and moving

---
+++ scripts/Env.cmd
@@ -0,0 +1,12 @@
+
+rem all paths are relative to the git scripts folder
+
+set appName=samples
+set majorVersion=5
+set minorVersion=1
+set collectionName=Blog
+set solutionName=aoBlogs2.sln
+set collectionPath=..\collections\blog\
+set binPath=..\source\aoblogs2\bin\debug\
+set msbuildLocation=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\
+set deploymentFolderRoot=C:\Users\jay\Desktop\deployments\aoBlog\Dev\
